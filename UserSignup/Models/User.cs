4eb1a0c8bccb80916b8806cd9a9347f9538ccff3
cperceful <cpercef1@gmail.com>
2017-07-13 17:49:45 -0500

    Built User class

--- UserSignup/Models/User.cs
+++ UserSignup/Models/User.cs
@@ -7,5 +7,38 @@ namespace UserSignup.Models
 {
     public class User
     {
+
+        private static int nextId = 1;
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public int UserId { get; } 
+
+        public User()
+        {
+            UserId = nextId;
+            nextId++;
+
+        }
+
+        public User(string username, string password) : base()
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public User(string username, string password, string email) : base()
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+        }
+
+
+
     }
 }
