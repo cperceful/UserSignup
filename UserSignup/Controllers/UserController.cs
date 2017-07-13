c26e842543288436e96b4adff3297c62b7e20ab9
cperceful <cpercef1@gmail.com>
2017-07-13 18:32:55 -0500

    Built Add POST method

--- UserSignup/Controllers/UserController.cs
+++ UserSignup/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UserSignup.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,11 +17,24 @@ namespace UserSignup.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Add()
         {
             ViewBag.title = "Add User";
             return View();
 
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user, string verify)
+        {
+            if (verify != user.Password)
+            {
+                ViewBag.error = "Passwords do not match";
+                return View();
+            }
+
+            return Redirect("/");
         }
     }
 }
