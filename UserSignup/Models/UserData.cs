a5a2136713115deefab4d9d83382adf4e7586e88
cperceful <cpercef1@gmail.com>
2017-07-13 19:37:40 -0500

    Built UserData class for handling logic of adding and finding users

---
+++ UserSignup/Models/UserData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserSignup.Models
+{
+    public class UserData
+    {
+
+        //List
+        private static List<User> users = new List<User>();
+
+        //Add
+        public void Add(User user)
+        {
+            users.Add(user);
+        }
+        
+        //GetAll
+        public List<User> GetAll()
+        {
+            return users;
+        }
+
+        //FindById
+        public User FindById(int userId)
+        {
+            return users.Single(x => x.UserId == userId);
+        }
+    }
+}
